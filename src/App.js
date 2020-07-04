cf5c7b9105b68e077e5467f9c412eeacdc04660e
diegobrunetto <diego.brunetto@hotmail.com>
2020-07-04 19:08:57 -0300

    delete some files and add favicon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Home from './pages/Home'
-/* import Search from './pages/Search' */
+import Search from './pages/Search'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </Router>
+
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/users">
+        <Users />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  {/*   <div className="App">
       <Home />
-      {/*    <Search /> */}
-    </div>
+      <Search /> 
+    </div > * /}
   );
 }
 
