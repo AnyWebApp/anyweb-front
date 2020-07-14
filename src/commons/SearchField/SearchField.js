275c019326268e48e3588e605a9a989f187a4f65
diegobrunetto <diego.brunetto@hotmail.com>
2020-07-14 20:41:30 -0300

    change styles route

--- src/commons/SearchField/SearchField.js
+++ src/commons/SearchField/SearchField.js
@@ -3,7 +3,7 @@ import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
-import { useStyles } from './SearchField.styles';
+import { useStyles } from './styles';
 
 export default function SearchField({ onSubmit, onChange, inputValue, placeholder }) {
   const classes = useStyles();
