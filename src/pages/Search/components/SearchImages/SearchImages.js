6326342db3efc1c27265b1dacfbab87b135ef468
diegobrunetto <diego.brunetto@hotmail.com>
2020-07-15 18:56:58 -0300

    new SearchImage component for the google searches

---
+++ src/pages/Search/components/SearchImages/SearchImages.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import SearchImagesCard from '../SearchImagesCard/SearchImagesCard';
+import { StyledDiv } from './styles';
+
+const SearchImages = () => {
+  return (
+    <StyledDiv>
+      <SearchImagesCard />
+      <SearchImagesCard />
+      <SearchImagesCard />
+      <SearchImagesCard />
+      <SearchImagesCard />
+      <SearchImagesCard />
+      <SearchImagesCard />
+      <SearchImagesCard />
+    </StyledDiv>
+  )
+};
+
+export default SearchImages;
---
+++ src/pages/Search/components/SearchImagesCard/SearchImagesCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+import { useStyles } from './styles'
+
+import perro1 from '../../../../assets/images/perro-1.png'
+import perro2 from '../../../../assets/images/perro-2.jpg'
+
+export default function SearchImagesCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={perro2}
+          title="Perro 1"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            www.perro1.com
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
