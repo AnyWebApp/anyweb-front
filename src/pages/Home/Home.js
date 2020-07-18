0787bb0c54a566090e4f1a86a7f2633a95e3a3f8
diegobrunetto <diego.brunetto@hotmail.com>
2020-07-17 23:10:12 -0300

    put token and data in userState

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,18 +8,15 @@ import Footer from '../../commons/Footer/Footer';
 import { HomeContainer, HomeMain, LogoContainer, LogoImg } from './styles';
 
 function Home() {
-
   const [inputValue, setInputValue] = useState('');
   const [isLogged, setisLogged] = useState(false);
-  const [userActiveLinks, setUserActiveLinks] = useState([])
   const [token, setToken] = useState('');
-  /* const token = sessionStorage.getItem('token'); */
-
+  const [userActiveLinks, setUserActiveLinks] = useState([])
   // todo a un .env
   const endpoint = 'https://any-web-backend.herokuapp.com/api/';
   const keyToken = 'dametoken';
   const keyPins = 'pins/';
-
+  //-----------
   const endpointToken = `${endpoint}${keyToken}`;
   const endpointPins = `${endpoint}${keyPins}${inputValue}`;
   const fetchPinOptions = {
@@ -30,7 +27,7 @@ function Home() {
       'Authorization': `Bearer ${token}`
     },
   };
-
+  //-------------------//
   const history = useHistory();
 
   useEffect(() => {
@@ -42,7 +39,6 @@ function Home() {
     const tokenResponse = await fetch(endpointToken);
     const newToken = await tokenResponse.json();
     setToken(newToken.token);
-    /*  sessionStorage.setItem('token', newToken.token); */
   };
 
   const fetchPin = async () => {
@@ -50,14 +46,7 @@ function Home() {
     const data = await pinResponse.json();
     if (pinResponse.status === 200) {
       setisLogged(true);
-      setUserActiveLinks(data)
-      console.log(pinResponse)
-
-      /* sessionStorage.setItem('data', data); */
-      /* const activeLinks = sessionStorage.getItem('data');
-      console.log(`data json: ${activeLinks}`)
-      const parse = JSON.parse(activeLinks)
-      console.log(`data in session: ${parse}`); */
+      setUserActiveLinks(data.pinData)
     }
   };
 
@@ -68,19 +57,19 @@ function Home() {
   const handlePinSubmit = (e) => {
     e.preventDefault();
     setInputValue('');
+    console.log(inputValue);
     fetchPin().catch(error => console.error('Pin Error:', error));
   }
 
   const handleSearchSubmit = () => {
     history.push("/search");
   }
-
   const handleSearchChange = (e) => {
     setInputValue(e.target.value)
   }
 
-  console.log(`token in session: ${token}`);
-  console.log(`data: ${userActiveLinks}`);
+  console.log(userActiveLinks);
+
   return (
     <HomeContainer>
       {isLogged ? <HomeNavBar color='primary' /> : <HomeNavBar color='secondary' />}
