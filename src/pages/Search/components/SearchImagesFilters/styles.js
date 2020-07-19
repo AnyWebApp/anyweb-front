0ab0326290d042067f9031a3db51ab5fc6550de2
diegobrunetto <diego.brunetto@hotmail.com>
2020-07-19 13:11:01 -0300

    Component for disply filter inimages searches

---
+++ src/pages/Search/components/SearchImagesFilters/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledDiv = styled.div`
+  display: flex;
+  width:100%;
+  height:40px;
+  align-items:center;
+  justify-content:space-around;
+  font-weight:400;
+  color:#727272;
+  font-size:14px;
+  border-bottom:1px solid lightgray;
+  cursor: pointer;
+`
