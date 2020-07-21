b1c1bb041fdc0ddab831b545a6b7672143421a74
diegobrunetto <diego.brunetto@hotmail.com>
2020-07-21 17:39:42 -0300

    file for constants

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const endpoint = process.env.REACT_APP_ENDPOINT;
+export const keyToken = process.env.REACT_APP_TOKEN;
+export const keyPins = process.env.REACT_APP_PIN;
+export const searchEndpoint = process.env.REACT_APP_SEARCHENDPOINT;
